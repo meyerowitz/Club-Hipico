152536d76186c6c6daf37c1b602195514a9501b0
unknown <meyerowitzrebeca@gmail.com>
2026-05-01 03:10:22 -0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/Club-Hipico/',,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
